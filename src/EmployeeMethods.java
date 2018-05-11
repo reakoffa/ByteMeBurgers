818992949869412cdbba85bc050c1dd8f65f010c
benjamin <benjamin.mcbrayer@gmail.com>
2018-05-11 10:31:55 -0400

    finished methods and populated managers and employees text files

--- src/EmployeeMethods.java
+++ src/EmployeeMethods.java
@@ -11,11 +11,12 @@ import java.util.ArrayList;
 
 public class EmployeeMethods {
 
-	// Read from the employee text file to generate an ArrayList of employees.
+	// Read from the managers text file to generate an ArrayList of managers (who
+	// will have permission to add/delete menu items and to add/delete employees).
 	public static ArrayList<Employee> readFromFileToArrayList(String filePath) {
-		ArrayList<Employee> employees = new ArrayList<>();		
+		ArrayList<Employee> employees = new ArrayList<>();
 		Path readFile = Paths.get(filePath);
-		
+
 		File file = readFile.toFile();
 
 		try {
@@ -33,36 +34,42 @@ public class EmployeeMethods {
 				line = reader.readLine();
 			}
 			reader.close();
-			
+
 		} catch (IOException e) {
 			System.out.println("Something went wrong!");
 		}
 		return employees;
 	}
-	
+
 	// Search an ArrayList of employees to validate userID.
-	// In Main method, first assign String userInput = Validator.getString(scnr, "Please enter your User ID number: ");
+	// In Main method, first assign String userInput = Validator.getString(scnr,
+	// "Please enter your User ID number: ");
 	public static void validateUserID(String userInput, ArrayList<Employee> employees) {
 		for (Employee e : employees) {
-			if (e.getName().equalsIgnoreCase(userInput)) {
-				System.out.println("\nOPTIONS\n1 - View the current menu\n2 - Add an item to the menu\n3 - Remove an item from the menu");
+			if (e.getId().equalsIgnoreCase(userInput)) {
+				System.out.println("Welcome, " + e.getName() + "!");
+				System.out.println(
+						"\nOPTIONS\n1 - View the current menu\n2 - Add an item to the menu\n3 - Remove an item from the menu");
 			} else {
 				System.out.println("ID not recognized. Please try again.");
 			}
 		}
 	}
-	
-	// In main method, first assign userNum = Validator.getInt(scnr, "Please select an option (1, 2, or 3): ", 1, 3);
-		// if (userNum == 1) {
-			//Display current menu.
-		//}
-		// if (userNum == 2) {
-			//  validate string. ask user to enter the name, category, description, and price of the new item (in that order, separated by a comma).
-			//  call method addItemToMenu.
-			// }
-		// if (userNum == 3) {
-			// validate string. ask user to enter the name of the food item to remove (will this be enough to delete the whole line?).
-			// call method deleteFoodItem.
+
+	// In main method, first assign userNum = Validator.getInt(scnr, "Please select
+	// an option (1, 2, or 3): ", 1, 3);
+	// if (userNum == 1) {
+	// Display current menu.
+	// }
+	// if (userNum == 2) {
+	// validate string. ask user to enter the name, category, description, and price
+	// of the new item (in that order, separated by a comma).
+	// call method addItemToMenu.
+	// }
+	// if (userNum == 3) {
+	// validate string. ask user to enter the name of the food item to remove (will
+	// this be enough to delete the whole line?).
+	// call method deleteFoodItem.
 	public static void addItemToMenu(FoodItem newfoodItem, String filePath) {
 
 		Path writeFile = Paths.get(filePath);
@@ -77,10 +84,13 @@ public class EmployeeMethods {
 			System.out.println("The file was not found here...");
 		}
 	}
-	
-	public static void deleteFoodItem(String itemToRemove, String dir, String originalFileName, String tempFileName) {
-		Path removeItemFromMenu = Paths.get(dir, originalFileName);
-		File file = removeItemFromMenu.toFile();
+
+	// Delete food item or delete employee.
+	// This method should work for deleting food items from a menu file and for
+	// deleting employees from a list.
+	public static void deleteFromFile(String itemToRemove, String dir, String originalFileName, String tempFileName) {
+		Path removeItemFromList = Paths.get(dir, originalFileName);
+		File file = removeItemFromList.toFile();
 		Path writeFile = Paths.get(dir, tempFileName);
 		File tempFile = writeFile.toFile();
 
@@ -112,9 +122,9 @@ public class EmployeeMethods {
 		} catch (IOException e) {
 			System.out.println("No need to panic but something's not right here.");
 		}
-	} 
+	}
 
-	// ADMINISTRATOR FUNCTION: Print out a list of employees.
+	// Print out a list of employees.
 	public static void printEmployeeList() {
 		ArrayList<Employee> employees = readFromFileToArrayList("resources/countries.txt");
 		int i = 0;
@@ -124,10 +134,10 @@ public class EmployeeMethods {
 		}
 	}
 
-	// TODO: Add employee to list.
-	public static void writeToFile(Employee employee, String dir, String fileName) {
+	// Add employee to list.
+	public static void writeToFile(Employee employee, String filePath) {
 
-		Path writeFile = Paths.get(dir, fileName);
+		Path writeFile = Paths.get(filePath);
 		File file = writeFile.toFile();
 
 		try {
